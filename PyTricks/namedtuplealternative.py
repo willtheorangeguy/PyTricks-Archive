f77f75e1be250dc5156f6fbb47b1cc62a3570975
William <willtheorangeguy@users.noreply.github.com>
2022-08-08 09:54:29 -0700

    Create namedtuplealternative.py

---
+++ PyTricks/namedtuplealternative.py
@@ -0,0 +1,20 @@
+# Why Python is Great: Namedtuples
+# Using namedtuple is way shorter than
+# defining a class manually:
+>>> from collections import namedtuple
+>>> Car = namedtuple('Car', 'color mileage')
+
+# Our new "Car" class works as expected:
+>>> my_car = Car('red', 3812.4)
+>>> my_car.color
+'red'
+>>> my_car.mileage
+3812.4
+
+# We get a nice string repr for free:
+>>> my_car
+Car(color='red' , mileage=3812.4)
+
+# Like tuples, namedtuples are immutable:
+>>> my_car.color = 'blue'
+AttributeError: "can't set attribute"
