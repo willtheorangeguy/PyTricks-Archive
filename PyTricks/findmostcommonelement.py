55274614b8f3e3e9ea3978e28bde47ad1f9c85b4
William <willtheorangeguy@users.noreply.github.com>
2022-08-08 09:48:29 -0700

    feat(findmostcommonelement): Create findmostcommonelement.py file with Counter implementation

---
+++ PyTricks/findmostcommonelement.py
@@ -0,0 +1,11 @@
+# collections.Counter lets you find the most common
+# elements in an iterable:
+
+>>> import collections
+>>> c = collections.Counter('helloworld')
+
+>>> c
+Counter({'l': 3, 'o': 2, 'e': 1, 'd': 1, 'h': 1, 'r': 1, 'w': 1})
+
+>>> c.most_common(3)
+[('l', 3), ('o', 2), ('e', 1)]
